91c10522b7e15fd6f2bf42f40522855a9de10193
Emil_HomePC <emilsd.1996@gmail.com>
2021-03-22 09:26:06 +0100

    An attempt solution for keeping track of the distance to the next obstacle (will probably be solved with triggers)

---
+++ Assets/Scripts/Endless Runner/NextObstacleQueue.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextObstacleQueue : MonoBehaviour
+{
+    /*private Queue<GameObject> nextObstacleQueue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextObstacleQueue = new Queue<GameObject>();
+
+        // Find the initial obstacles (There are two)
+        ObstacleHit[] initialObstacles = FindObjectsOfType<ObstacleHit>();
+        nextObstacleQueue.Enqueue(initialObstacles[1].gameObject);
+        nextObstacleQueue.Enqueue(initialObstacles[0].gameObject);
+    }
+
+    public void Enqueue(GameObject gameObject)
+    {
+        nextObstacleQueue.Enqueue(gameObject);
+    }
+
+    public GameObject Dequeue()
+    {
+        return nextObstacleQueue.Dequeue();
+    }*/
+}
