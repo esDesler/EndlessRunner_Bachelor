62ceb19cce3df187c9eede76f424314d66df1ccb
Emil_HomePC <emilsd.1996@gmail.com>
2021-03-22 16:02:32 +0100

    Added an cutoff offset to adjust when the audio cuts off.

--- Assets/Scripts/Endless Runner/AudioCutoff.cs
+++ Assets/Scripts/Endless Runner/AudioCutoff.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AudioCutoff : MonoBehaviour
 {
+    // To find the sweet spot for the cutoff. 
+    public float cutoffOffset; 
+
     private GameObject thePlayer;
     private AudioSource myAudioSource;
     // Start is called before the first frame update
@@ -16,7 +19,7 @@ public class AudioCutoff : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (thePlayer.transform.position.z > transform.position.z)
+        if (thePlayer.transform.position.z > transform.position.z + cutoffOffset)
         {
             myAudioSource.Stop();
         }
