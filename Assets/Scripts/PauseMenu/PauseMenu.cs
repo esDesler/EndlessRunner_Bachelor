8a430f79faea24de89415ff8f5e7fc919603d3dc
Emil_HomePC <emilsd.1996@gmail.com>
2021-03-22 16:06:19 +0100

    Now pauses the game sounds when in the pause screen

--- Assets/Scripts/PauseMenu/PauseMenu.cs
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -2,28 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public string mainMenu;
 
     public GameObject thePauseMenu;
+
+    public Button theResumeButton;
+
+    private AudioSource endlessRunnerMusic;
+    private AudioSource MenuHover;
+    private AudioSource MenuArrowUp;
+    private AudioSource MenuClick;
+
+    private void Awake()
+    {
+        endlessRunnerMusic = GameObject.Find("EndlessRunnerMusic").GetComponent<AudioSource>();
+        MenuHover = GameObject.Find("Menu_Hover").GetComponent<AudioSource>();
+        MenuArrowUp = GameObject.Find("Menu_Arrow_Up").GetComponent<AudioSource>();
+        MenuClick = GameObject.Find("Menu_Click").GetComponent<AudioSource>();
+
+
+        endlessRunnerMusic.ignoreListenerPause = true;
+        MenuHover.ignoreListenerPause = true;
+        MenuArrowUp.ignoreListenerPause = true;
+        MenuClick.ignoreListenerPause = true;
+    }
+
     public void PauseGame()
     {
         Time.timeScale = 0f;
         thePauseMenu.SetActive(true);
+        theResumeButton.Select();
+        PauseGameMusic(true);
     }
 
     public void ResumeGame()
     {
         thePauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        PauseGameMusic(false);
     }
 
     public void RestartGame()
     {
         thePauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        PauseGameMusic(false);
         FindObjectOfType<GameManager>().Reset();
     }
 
@@ -31,6 +58,12 @@ public class PauseMenu : MonoBehaviour
     {
         thePauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        PauseGameMusic(false);
         SceneManager.LoadScene(mainMenu);
+    }
+
+    private void PauseGameMusic(bool state)
+    {
+        AudioListener.pause = state;
     }
 }
